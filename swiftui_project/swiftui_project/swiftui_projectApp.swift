4de8899e02f5aa06384ee1d1ca2e2b2a67a7315c
badanov_max <badanovmaxx@gmail.com>
2023-11-07 14:12:16 +0300

    создан проект с тестами и  Core Data

---
+++ swiftui_project/swiftui_project/swiftui_projectApp.swift
@@ -0,0 +1,20 @@
+//
+//  swiftui_projectApp.swift
+//  swiftui_project
+//
+//  Created by Максим Баданов on 07.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_projectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
