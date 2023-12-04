bb6dd56f2f2ef62c62ca2a61ef9eaca87469196a
badanov_max <badanovmaxx@gmail.com>
2023-12-04 20:10:43 +0300

    коммит

--- swiftui_project/swiftui_project/StartScreen/LogoView.swift
+++ swiftui_project/swiftui_project/StartScreen/LogoView.swift
@@ -5,16 +5,16 @@ struct LogoView: View {
     var body: some View {
         VStack(spacing: UISize.size24) {
             HStack(spacing: UISize.size8) {
-                CustomText(text: Logo.movy.name)
+                Text(Logo.movy.name)
                     .foregroundColor(.customGray)
-                CustomText(text: ".")
+                Text(".")
                     .foregroundColor(.customRed)
             }
             .textStyle(size: UISize.size64,
                        weight: .bold)
             VStack(spacing: UISize.size8) {
-                CustomText(text: "Choose your best movie for")
-                CustomText(text: "tonight quickly.")
+                Text("Choose your best movie for")
+                Text("tonight quickly.")
             }
             .textStyle(size: UISize.size24,
                        weight: .regular)
--- swiftui_project/swiftui_project/Supports&Extensions/CustomText.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-
-struct CustomText: View {
-    let text: String
-    var body: some View {
-        Text(text)
-    }
-}
