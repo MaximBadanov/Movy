e4b5b8ec869aa2b99a436651f90fc23e33b1ff52
badanov_max <badanovmaxx@gmail.com>
2024-01-07 15:04:48 +0300

    все

--- swiftui_project/swiftui_project/Supports&Extensions/Genre/GenreSupports/GenreButtonExtension.swift
+++ swiftui_project/swiftui_project/Supports&Extensions/Genre/GenreSupports/GenreButtonExtension.swift
@@ -3,9 +3,11 @@ import SwiftUI
 
 extension View {
     func genreButtonStyle(backgroundColor: Color, isSelected: Bool) -> some View {
-        buttonStyle(GenreButtonStyle(
-            backgroundColor: backgroundColor,
-            isSelected: isSelected)
+        buttonStyle(
+            GenreButtonStyle(
+                backgroundColor: backgroundColor,
+                isSelected: isSelected
+            )
         )
     }
 }
