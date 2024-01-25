13930027589e59d843dbd07d71a6ab98a4cd55d4
badanov_max <badanovmaxx@gmail.com>
2024-01-25 22:49:45 +0300

    проба пера

---
+++ swiftui_project/swiftui_project/NetworkService/NetworkService.swift
@@ -0,0 +1,12 @@
+
+class NetworkService: NetworkServiceProtocol {
+    func fetchMovies() {
+        print("получили кучу фильмов")
+    }
+    
+    func decodeImage() {
+        print("декодировали url в image")
+    }
+    
+    
+}
---
+++ swiftui_project/swiftui_project/NetworkService/NetworkServiceProtocol.swift
@@ -0,0 +1,4 @@
+protocol NetworkServiceProtocol {
+    func fetchMovies()
+    func decodeImage()
+}
