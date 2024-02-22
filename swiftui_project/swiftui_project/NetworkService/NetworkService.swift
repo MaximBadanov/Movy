ff28bac34370d74ddb35763c42d8d72027da6b96
badanov_max <badanovmaxx@gmail.com>
2024-02-22 16:48:44 +0300

    ван мо

--- swiftui_project/swiftui_project/NetworkService/NetworkService.swift
+++ swiftui_project/swiftui_project/NetworkService/NetworkService.swift
@@ -1,4 +1,6 @@
 import Foundation
+
+
 class NetworkService: NetworkServiceProtocol {
     func makeRequest<T: Decodable>(requestModel: RequestModel<T>) {
         let header = requestModel.header
@@ -10,12 +12,14 @@ class NetworkService: NetworkServiceProtocol {
             if error != nil {
                 print("requestJSON error")
             }
-            guard let data = data else { return }
+            guard let data = data else { print("No data"); return }
             guard let response = response as? HTTPURLResponse else { fatalError() }
             do {
 //                пока оставил для проверки работы от сюда
-                let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
-                let decodedData = try JSONDecoder().decode(requestModel.modelToParse.self, from: data)
+                let json = try JSONSerialization.jsonObject(with: data,
+                                                            options: .mutableContainers)
+                let decodedData = try JSONDecoder().decode(requestModel.modelToParse.self,
+                                                           from: data)
                 print("requestJSON success")
                 print(decodedData)
                 print(json)
