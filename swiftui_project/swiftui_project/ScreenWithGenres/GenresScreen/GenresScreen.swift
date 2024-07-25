be193ce3487e45dfebf86ce3350e4f0cb0652db3
badanov_max <badanovmaxx@gmail.com>
2024-07-25 23:35:07 +0300

    navigation jump fixed

--- swiftui_project/swiftui_project/ScreenWithGenres/GenresScreen/GenresScreen.swift
+++ swiftui_project/swiftui_project/ScreenWithGenres/GenresScreen/GenresScreen.swift
@@ -5,32 +5,30 @@ struct GenresScreen: View {
     @ObservedObject private var genresWithScrollViewModel = GenresWithScrollViewModel()
     
     var body: some View {
-        NavigationStack {
-            VStack(alignment: .leading, spacing: UISize.size8) {
+        VStack(alignment: .leading, spacing: UISize.size8) {
+            Spacer()
+            Text("Choose Genres")
+                .padding(.leading, UISize.size24)
+                .textStyle(size: UISize.size32, weight: .bold)
+            GenresViewWithScroll(viewModel: genresWithScrollViewModel)
+            Spacer()
+            HStack(spacing: UISize.size8) {
                 Spacer()
-                Text("Choose Genres")
-                    .padding(.leading, UISize.size24)
-                    .textStyle(size: UISize.size32, weight: .bold)
-                GenresViewWithScroll(viewModel: genresWithScrollViewModel)
-                Spacer()
-                HStack(spacing: UISize.size8) {
-                    Spacer()
-                        .padding(.horizontal, UISize.size24)
-                    Button("Get Movie",
-                           action: {
-                            genreScreenViewModel.fetchMoviesByGenre()
-                    })
-                    .buttonStyle(.primaryStyle)
-                    .padding(.trailing, UISize.size24)
-                    .navigationDestination(
-                        isPresented: $genreScreenViewModel.navigateToResult,
-                        destination: { ResultView(
-                            title: genreScreenViewModel.movie?.title ?? "no title",
-                            poster: genreScreenViewModel.movie?.poster ?? "no poster",
-                            genres: genreScreenViewModel.stringOfGenres)
-                        }
-                    )
-                }
+                    .padding(.horizontal, UISize.size24)
+                Button("Get Movie",
+                       action: {
+                    genreScreenViewModel.fetchMoviesByGenre()
+                })
+                .buttonStyle(.primaryStyle)
+                .padding(.trailing, UISize.size24)
+                .navigationDestination(
+                    isPresented: $genreScreenViewModel.navigateToResult,
+                    destination: { ResultView(
+                        title: genreScreenViewModel.movie?.title ?? "no title",
+                        poster: genreScreenViewModel.movie?.poster ?? "no poster",
+                        genres: genreScreenViewModel.stringOfGenres)
+                    }
+                )
             }
             .padding(.bottom, UISize.size16)
         }
