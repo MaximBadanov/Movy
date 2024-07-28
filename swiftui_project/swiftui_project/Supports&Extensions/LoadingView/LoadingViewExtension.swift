05a849fc447986dd412ec54032799620cc02d0e1
badanov_max <badanovmaxx@gmail.com>
2024-07-28 14:34:53 +0300

    small premerge changes

--- swiftui_project/swiftui_project/Supports&Extensions/LoadingView/LoadingViewExtension.swift
+++ swiftui_project/swiftui_project/Supports&Extensions/LoadingView/LoadingViewExtension.swift
@@ -3,10 +3,9 @@ import SwiftUI
 extension View {
     @ViewBuilder
     func isLoading(isLoading: Bool) -> some View {
-        switch isLoading {
-        case true:
+        if isLoading {
             LoadingView()
-        case false:
+        } else {
             self
         }
     }
