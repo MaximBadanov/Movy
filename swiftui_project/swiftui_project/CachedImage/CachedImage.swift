378624f0e70dc256a03772b12ae4de3b365f3718
badanov_max <badanovmaxx@gmail.com>
2024-10-13 16:30:05 +0300

    all comments fixed, cache service implemented

--- swiftui_project/swiftui_project/CachedImage/CachedImage.swift
+++ swiftui_project/swiftui_project/CachedImage/CachedImage.swift
@@ -6,19 +6,19 @@ struct CachedImage: View {
     @StateObject private var viewModel = CachedImageViewModel()
     
     var body: some View {
-        if let cachedImage = viewModel.downloadedImage {
-            Image(uiImage: cachedImage)
-                .resizable()
-                .frame(maxWidth: .infinity)
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(UISize.size16)
-        } else if viewModel.isLoading {
-            PlaceholderImage()
-                .onAppear {
-                    viewModel.loadImage(from: url)
-                }
-        } else if  viewModel.hasError {
-            PlaceholderImage()
+        ZStack {
+            if let cachedImage = viewModel.downloadedImage {
+                Image(uiImage: cachedImage)
+                    .resizable()
+                    .frame(maxWidth: .infinity)
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(UISize.size16)
+            } else {
+                PlaceholderImage()
+            }
+        }
+        .onAppear {
+            viewModel.loadImage(from: url)
         }
     }
 }
